b425047d9e8cc16691c8b64cb47f26456d268749
Hysilensfan <uraume2027@gmail.com>
2026-01-30 14:21:51 +0800

    Create bool&basic operator.py

---
+++ bool&basic operator.py
@@ -0,0 +1,12 @@
+a = 15 + 2; print(a)      # addition: calculate 15 + 2 and print result
+a = 15 - 2; print(a)      # subtraction: calculate 15 - 2 and print result
+a = 15 / 2; print(a)      # Division: calculate 15 ÷ 2 (float Division)
+a = 15 // 2; print(a)     # Divisible: quotient of 15 Divisible by 2
+a = 15 % 2; print(a)      # remainder: remainder of 15 divided by 2
+a = 15 ** 2; print(a)     # exponentiation: 15 squared
+pi = float(3.14)          # float: define pi
+print(5 ** 2 * pi)        # calculate circle area: 5 squared times pi
+o = int(000777)           # integer: 15 convert to integer
+print(str(o))             # convert to string: convert the integer 'o' to a string and print result
+c: bool = True            # bool value: define c is True
+print("Y" if c else "N")  # conditional expression: if c is True print Y else  print N
