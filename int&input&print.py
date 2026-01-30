fb482c3d58bdbe984099e158eed5c696ea52b0e8
Hysilensfan <uraume2027@gmail.com>
2026-01-30 11:44:42 +0800

    Update int&input&print.py

--- int&input&print.py
+++ int&input&print.py
@@ -1,5 +1,5 @@
 a=1;print(a) # print the value of a
 a,b=1,2;print(a+b) # print the sum of a and b
-a,b,c=1,-2,1;print((-b+((b**2-4*a*c)**0.5))/(2*a)) #two roots of Quadratic equation in one variable
+a,b,c=1,-2,1;print(f"{(-b+((b**2-4*a*c)**0.5))/(2*a)}{(-b-((b**2-4*a*c)**0.5))/(2*a)}") #two roots of Quadratic equation in one variable
 d=int(input());print(d) # read input, convert it to integer, then print it
 print(sum(int(g) for g in input().split())) # input something and parting them,convert each value to integer,then sum them
