fbac89a879c383b000170b71f1df1040d18bec34
Hysilensfan <uraume2027@gmail.com>
2026-02-02 11:58:48 +0800

    Create class basic.py

---
+++ class basic.py
@@ -0,0 +1,26 @@
+#Code Comparison(just using define function)
+def add(self,i,j):
+    self.result=i*j;print(" ".join(f"{i}*{j}={self.result}"),end="  ")
+mt = Multiplicationtable();print(f"there are the multiplication table: ")
+for index in range(81):
+    i, j = index // 9 + 1 ,index % 9 + 1
+    mt.add(i, j)
+    while j == 9:
+        print()
+        break
+add = lambda i,j:print(" ".join(f"{i}*{j}={i*j}"),end="  ")
+print(f"there are the multiplication table: ")
+for index in range(81):
+    i, j = index // 9 + 1 ,index % 9 + 1;add(i, j)
+    while j == 9:print();break
+#Code Comparison(using class)
+class Multiplicationtable(object):
+    def __init__(self):
+        self.staringvalueI=1;self.staringvalueII=1;self.result=0
+    def add(self):
+        self.result=self.staringvalueI * self.staringvalueII;print(" ".join(f"{self.staringvalueI}*{self.staringvalueII}={self.result}"),end="  ")
+mt = Multiplicationtable();print(f"there are the multiplication table: ")
+for _ in range(9):
+    for _ in range(9):
+        mt.add();mt.staringvalueII +=1
+    print();mt.staringvalueII = 1;mt.staringvalueI+=1
