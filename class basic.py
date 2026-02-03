5926833625bbb271c42655f7cc61ed8d221c3f90
Hysilensfan <uraume2027@gmail.com>
2026-02-03 11:58:24 +0800

    Update class basic.py

--- class basic.py
+++ class basic.py
@@ -7,6 +7,7 @@ for index in range(81):
 
 
 #Code Comparison(using class)
+#ex1:
 class Multiplicationtable(object):
     def __init__(self):
         self.staringvalueI=1;self.staringvalueII=1;self.result=0
@@ -17,3 +18,24 @@ for _ in range(9):
     for _ in range(9):
         mt.add();mt.staringvalueII +=1
     print();mt.staringvalueII = 1;mt.staringvalueI+=1
+
+
+#ex2:
+class Archer:
+    def __init__(self):
+        self.bone = "sword"
+        self.body = "steel"
+        self.blood = "fire"
+        self.blades_created = 1000
+        self.alive = False
+        self.dead = False
+
+    def unlimited_blade_works(self):
+        print(f"I am the bone of my {self.bone}.")
+        print(f"{self.body} is my body, and {self.blood} is my blood.")
+        print(f"I have created over {self.blades_created} blades, "
+        print(f"Unknown to Death, Nor known to Life"if not self.alive and not self.dead)
+        print("As I pray...")
+        print("Unlimited Blade Works!")
+archer = Archer()
+archer.unlimited_blade_works()
